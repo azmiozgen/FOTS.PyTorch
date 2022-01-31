Fix precision, recall, f1 metrics; add text accuracy and value errors, set detailed Tensorboard outputs

FOTS/base/base_trainer.py
@@ -22,7 +22,7 @@ class BaseTrainer:
         self.epochs = config['trainer']['epochs']
         self.save_freq = config['trainer']['save_freq']
         self.verbosity = config['trainer']['verbosity']
-        self.summyWriter = SummaryWriter()
+        self.summary_writer = SummaryWriter()
 
         if torch.cuda.is_available():
             if config['cuda']:
@@ -107,26 +107,31 @@ class BaseTrainer:
                 self.train_logger.add_entry(log)
                 if self.verbosity >= 1:
                     for key, value in log.items():
-                        self.logger.info('    {:15s}: {}'.format(str(key), value))
+                        if str(key) == 'epoch':
+                            self.logger.info('    {:15s}: {}'.format(str(key), value))
+                        else:
+                            self.logger.info('    {:15s}: {:.4f}'.format(str(key), value))
             if (self.monitor_mode == 'min' and log[self.monitor] < self.monitor_best)\
                     or (self.monitor_mode == 'max' and log[self.monitor] > self.monitor_best):
                 self.monitor_best = log[self.monitor]
-                self._save_checkpoint(epoch, log, save_best=True)
+                # self._save_checkpoint(epoch, log, save_best=True)
+                pass
             if epoch % self.save_freq == 0:
-                self._save_checkpoint(epoch, log)
+                # self._save_checkpoint(epoch, log)
+                pass
             if self.lr_scheduler:
                 self.lr_scheduler.step()
                 lr = self.lr_scheduler.get_lr()[0]
                 self.logger.info('New Learning Rate: {:.8f}'.format(lr))
 
-            self.summyWriter.add_scalar('Train_loss', result['loss'], epoch)
-            self.summyWriter.add_scalar('Train_precision', result['precision'], epoch)
-            self.summyWriter.add_scalar('Train_recall', result['recall'], epoch)
-            self.summyWriter.add_scalar('Train_hmean_f1', result['hmean'], epoch)
-            self.summyWriter.add_scalar('Val_precision', result['val_precision'], epoch)
-            self.summyWriter.add_scalar('Val_recall', result['val_recall'], epoch)
-            self.summyWriter.add_scalar('Val_hmean_f1', result['val_hmean'], epoch)
-        self.summyWriter.close()
+            self.summary_writer.add_scalar('Train_loss', result['loss'], epoch)
+            self.summary_writer.add_scalar('Train_precision', result['precision'], epoch)
+            self.summary_writer.add_scalar('Train_recall', result['recall'], epoch)
+            self.summary_writer.add_scalar('Train_hmean_f1', result['hmean'], epoch)
+            self.summary_writer.add_scalar('Val_precision', result['val_precision'], epoch)
+            self.summary_writer.add_scalar('Val_recall', result['val_recall'], epoch)
+            self.summary_writer.add_scalar('Val_hmean_f1', result['val_hmean'], epoch)
+        self.summary_writer.close()
 
     def _log_memory_useage(self):
         if not self.with_cuda: return

FOTS/data_loader/dataset.py
@@ -12,7 +12,7 @@ from torch.utils.data import Dataset
 logger = logging.getLogger(__name__)
 
 class PriceTagDataset(Dataset):
-    def __init__(self, data_root, image_ext='jpg', json_ext='json'):
+    def __init__(self, data_root, image_ext='jpg', json_ext='json', input_size=256):
         images_dir = os.path.join(data_root, 'images')
         annotations_dir = os.path.join(data_root, 'annotations')
         self.image_files = sorted(glob.glob(os.path.join(images_dir, '*.' + image_ext)))
@@ -25,6 +25,7 @@ class PriceTagDataset(Dataset):
             self.transcriptions.append(transcription)
         self.visualization_dir = os.path.join(data_root, 'visualization')
         os.makedirs(self.visualization_dir, exist_ok=True)
+        self.input_size = input_size
 
     def _load_annotation(self, ann_file, keys=['price', 'price_cent'],
                 bbox_name='bbox', content_name='content', delimiter=','):
@@ -105,7 +106,7 @@ class PriceTagDataset(Dataset):
     def __len__(self):
         return len(self.image_files)
 
-    def __transform(self, gt, input_size=512, random_scale=[0.5, 1, 2.0, 3.0],
+    def __transform(self, gt, random_scale=[0.5, 1, 2.0, 3.0],
             geo_map_channels=5):
         image_file, bbox, transcription = gt
         image = cv2.imread(image_file)
@@ -121,15 +122,15 @@ class PriceTagDataset(Dataset):
 
         ## Resize image
         _h, _w, _ = image.shape
-        image = cv2.resize(image, dsize=(input_size, input_size))
-        ratio_w = input_size / _w
-        ratio_h = input_size / _h
+        image = cv2.resize(image, dsize=(self.input_size, self.input_size))
+        ratio_w = self.input_size / _w
+        ratio_h = self.input_size / _h
         bbox *= np.array([ratio_w, ratio_h, ratio_w, ratio_h])
 
         ## Set score map, geo map and training mask
-        score_map = np.zeros((input_size, input_size), dtype=np.uint8)
-        geo_map = np.zeros((input_size, input_size, geo_map_channels), dtype=np.float32)
-        training_mask = np.ones((input_size, input_size), dtype=np.uint8)  ## One region
+        score_map = np.zeros((self.input_size, self.input_size), dtype=np.uint8)
+        geo_map = np.zeros((self.input_size, self.input_size, geo_map_channels), dtype=np.float32)
+        training_mask = np.ones((self.input_size, self.input_size), dtype=np.uint8)  ## One region
 
         ## Mask score map and geo map by bbox
         _bbox = bbox.astype(np.int32)

FOTS/trainer/trainer.py
@@ -56,31 +56,29 @@ class Trainer(BaseTrainer):
         self.model.train()
 
         total_loss = 0
-        total_metrics = np.zeros(3) # precious, recall, hmean
+        total_metrics = np.zeros(3) # precision, recall, hmean
+        text_accuracy = 0.0
+        value_error = 0.0
         for batch_idx, gt in enumerate(self.data_loader):
             try:
-                imagePaths, img, score_map, geo_map, training_mask, transcripts, boxes, mapping = gt
-                img, score_map, geo_map, training_mask = self._to_tensor(img, score_map, geo_map, training_mask)
+                image_files, image, score_map, geo_map, training_mask, transcriptions, boxes, mapping = gt
+                image, score_map, geo_map, training_mask = self._to_tensor(image, score_map, geo_map, training_mask)
 
                 # import cv2
                 # for i in range(img.shape[0]):
                 #     image = img[i]
                 #     for tt, bb in zip(transcripts[i], boxes[i]):
                 #         show_box(image.permute(1, 2, 0).detach().cpu().numpy()[:,:, ::-1].astype(np.uint8).copy(), bb, tt)
-                img_grid = torchvision.utils.make_grid(img)
-                score_map_grid = torchvision.utils.make_grid(score_map)
-                self.summyWriter.add_image('images', img_grid, epoch * len(self.data_loader) + batch_idx)
-                self.summyWriter.add_image('masks', score_map_grid, epoch * len(self.data_loader) + batch_idx)
 
                 self.optimizer.zero_grad()
-                pred_score_map, pred_geo_map, pred_recog, pred_boxes, pred_mapping, indices = self.model.forward(img, boxes, mapping)
+                pred_score_map, pred_geo_map, pred_recog, pred_boxes, pred_mapping, indices = self.model.forward(image, boxes, mapping)
                 #import pdb; pdb.set_trace()
-                transcripts = transcripts[indices]
+                transcriptions = transcriptions[indices]
                 pred_boxes = pred_boxes[indices]
                 pred_mapping = mapping[indices]
-                pred_fns = [imagePaths[i] for i in pred_mapping]
+                pred_fns = [image_files[i] for i in pred_mapping]
 
-                labels, label_lengths = self.labelConverter.encode(transcripts.tolist())
+                labels, label_lengths = self.labelConverter.encode(transcriptions.tolist())
                 labels = labels.to(self.device)
                 label_lengths = label_lengths.to(self.device)
                 recog = (labels, label_lengths)
@@ -91,7 +89,7 @@ class Trainer(BaseTrainer):
                 self.optimizer.step()
 
                 total_loss += loss.item()
-                pred_transcripts = []
+                pred_transcriptions = []
                 if len(pred_mapping) > 0:
                     pred, lengths = pred_recog
                     _, pred = pred.max(2)
@@ -99,12 +97,44 @@ class Trainer(BaseTrainer):
                         l = lengths[i]
                         p = pred[:l, i]
                         t = self.labelConverter.decode(p, l)
-                        pred_transcripts.append(t)
-                    pred_transcripts = np.array(pred_transcripts)
+                        pred_transcriptions.append(t)
+                    pred_transcriptions = np.array(pred_transcriptions)
+
+                if epoch % self.save_freq == 0 and batch_idx == 0:
+                    image_grid = torchvision.utils.make_grid(image)
+                    score_map_grid = torchvision.utils.make_grid(score_map)
+                    pred_score_map_grid = torchvision.utils.make_grid(pred_score_map)
+                    step = epoch * len(self.data_loader) + batch_idx
+                    gt_transriptions_str =  ' '.join(transcriptions)
+                    pred_transcriptions_str = ' '.join(pred_transcriptions)
+                    self.summary_writer.add_image('images', image_grid, step)
+                    self.summary_writer.add_image('gt_masks', score_map_grid, step)
+                    self.summary_writer.add_image('pred_masks', pred_score_map_grid, step)
+                    self.summary_writer.add_text('gt_transcriptions', gt_transriptions_str, step)
+                    self.summary_writer.add_text('pred_transcriptions', pred_transcriptions_str, step)
 
                 gt_fns = pred_fns
-                total_metrics += self._eval_metrics((pred_boxes, pred_transcripts, pred_fns),
-                                                        (boxes, transcripts, gt_fns))
+                print('PRED TRANSCRIPTIONS', pred_transcriptions)
+                print('GT TRANSCRIPTIONS', transcriptions)
+                total_metrics += self._eval_metrics((pred_boxes, pred_transcriptions, pred_fns),
+                                                        (boxes, transcriptions, gt_fns))
+                # print('TOTAL METRICS', total_metrics)
+
+                ## Transcripton accuracy
+                text_accuracy += np.mean(transcriptions == pred_transcriptions)
+
+                ## Value error
+                try:
+                    batch_value_error = 0.0
+                    for tr, pred_tr in zip(transcriptions, pred_transcriptions):
+                        tr1, tr2 = tr.split(',')
+                        tr_value = int(tr1) + int(tr2) / 100.0
+                        pred_tr1, pred_tr2 = pred_tr.split(',')
+                        pred_tr_value = int(pred_tr1) + int(pred_tr2) / 100.0
+                        batch_value_error += abs(tr_value - pred_tr_value) / (tr_value)
+                    value_error += (batch_value_error / len(transcriptions))
+                except ValueError:
+                    value_error += 1.0
 
                 if self.verbosity >= 2 and batch_idx % self.log_step == 0:
                     self.logger.info('Train Epoch: {} [{}/{} ({:.0f}%)] Loss: {:.6f} IOU Loss: {:.6f} CLS Loss: {:.6f} Recognition Loss: {:.6f}'.format(
@@ -114,14 +144,16 @@ class Trainer(BaseTrainer):
                         100.0 * batch_idx / len(self.data_loader),
                         loss.item(), iou_loss.item(), cls_loss.item(), reg_loss.item()))
             except:
-                print(imagePaths)
+                print('Training failed')
                 raise
 
         log = {
             'loss': total_loss / len(self.data_loader),
             'precision': total_metrics[0] / len(self.data_loader),
             'recall': total_metrics[1] / len(self.data_loader),
-            'hmean': total_metrics[2] / len(self.data_loader)
+            'hmean': total_metrics[2] / len(self.data_loader),
+            'text_accuracy': text_accuracy / len(self.data_loader),
+            'value_error': value_error / len(self.data_loader)
         }
 
         if self.valid:

train.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
     else:
         if args.resume is not None:
             logger.warning('Warning: --config overridden by --resume')
-            config = torch.load(args.resume, map_location = 'cpu')['config']
+            config = torch.load(args.resume, map_location='cpu')['config']
 
     assert config is not None
 