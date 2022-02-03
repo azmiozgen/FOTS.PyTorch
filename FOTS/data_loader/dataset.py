Add random rotation, fix validation mode augmentation, go -[1, 1] normalization

FOTS/data_loader/dataset.py
@@ -1,15 +1,21 @@
 import glob
+import json
 import logging
+import os
+import random
 
-from .datautils import *
+import cv2
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 from torch.utils.data import Dataset
 
+from .datautils import (clip_box, denormalize, get_corners, get_enclosing_box,
+        rotate_image, rotate_box)
+
 logger = logging.getLogger(__name__)
 
 class PriceTagDataset(Dataset):
-    def __init__(self, data_root, image_ext='jpg', json_ext='json', input_size=256):
+    def __init__(self, data_root, train_mode=True, image_ext='jpg', json_ext='json', input_size=256):
         images_dir = os.path.join(data_root, 'images')
         annotations_dir = os.path.join(data_root, 'annotations')
         self.image_files = sorted(glob.glob(os.path.join(images_dir, '*.' + image_ext)))
@@ -23,6 +29,7 @@ class PriceTagDataset(Dataset):
         self.visualization_dir = os.path.join(data_root, 'visualization')
         os.makedirs(self.visualization_dir, exist_ok=True)
         self.input_size = input_size
+        self.train_mode = train_mode
 
     def _load_annotation(self, ann_file, keys=['price', 'price_cent'],
                 bbox_name='bbox', content_name='content', delimiter=','):
@@ -71,19 +78,16 @@ class PriceTagDataset(Dataset):
         bbox = self.bboxes[index]
         transcription = self.transcriptions[index]
 
+        if visualize:
+            self.visualize(image_file, bbox, transcription)
+
         image_file, image, score_map, training_mask, transcription, bbox = \
                 self.__transform((image_file, bbox, transcription))
 
         if visualize:
-            # print('image_file:', image_file)
-            # print('images', image.shape)
-            # print('transcriptions', transcription)
-            # print('bboxes', bbox)
-            # print('score_maps', score_map.shape)
-            # print('geo_maps', geo_map.shape)
-            # print('training_masks', training_mask.shape)
             transformed_image_filename = image_filename_wo_ext + '_transformed.' + ext
             transformed_image_file = os.path.join(self.visualization_dir, transformed_image_filename)
+            image = denormalize(image, from_min=-1, from_max=0, to_min=0, to_max=255)
             cv2.imwrite(transformed_image_file, cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
             self.visualize(transformed_image_file, bbox, transcription[0])
 
@@ -92,7 +96,9 @@ class PriceTagDataset(Dataset):
     def __len__(self):
         return len(self.image_files)
 
-    def __transform(self, gt, random_scale=[0.5, 1, 2.0, 3.0]):
+    def __transform(self, gt,
+            random_scales=[0.5, 1, 2.0, 3.0],
+            random_rotation_angles=[-10, 11]):
         image_file, bbox, transcription = gt
         image = cv2.imread(image_file)
         h, w = image.shape[:2]
@@ -102,19 +108,25 @@ class PriceTagDataset(Dataset):
         x1, y1, x2, y2 = bbox
 
         ## Random crop without losing bbox
-        top_crop = int(np.random.random() * y1)
-        left_crop = int(np.random.random() * x1)
-        bottom_crop = int(np.random.random() * (h - y2))
-        right_crop = int(np.random.random() * (w - x2))
-        image = image[top_crop : h - bottom_crop, left_crop : w - right_crop, :]
-        bbox = np.array([x1 - left_crop, y1 - top_crop, x2 - left_crop, y2 - top_crop])
+        if self.train_mode:
+            top_crop = int(np.random.random() * y1)
+            left_crop = int(np.random.random() * x1)
+            bottom_crop = int(np.random.random() * (h - y2))
+            right_crop = int(np.random.random() * (w - x2))
+            image = image[top_crop : h - bottom_crop, left_crop : w - right_crop, :]
+            bbox = np.array([x1 - left_crop, y1 - top_crop, x2 - left_crop, y2 - top_crop])
 
-        # Random scale
-        rd_scale = np.random.choice(random_scale)
-        image = cv2.resize(image, dsize=None, fx=rd_scale, fy=rd_scale, interpolation=cv2.INTER_NEAREST)
-        bbox *= rd_scale
+            # Random scale
+            rd_scale = np.random.choice(random_scales)
+            image = cv2.resize(image, dsize=None, fx=rd_scale, fy=rd_scale, interpolation=cv2.INTER_NEAREST)
+            bbox *= rd_scale
 
-        ## Resize image
+            ## Random rotation
+            rotation_angle = np.random.randint(*random_rotation_angles)
+            image, bboxes = RandomRotate(angle=rotation_angle)(image, bbox.reshape(1, -1))
+            bbox = bboxes[0]
+
+        ## Resize image to input size
         _h, _w, _ = image.shape
         image = cv2.resize(image, dsize=(self.input_size, self.input_size), interpolation=cv2.INTER_NEAREST)
         ratio_w = self.input_size / _w
@@ -135,7 +147,8 @@ class PriceTagDataset(Dataset):
         bbox = [x1, y1, x2, y2, x3, y3, x4, y4]
 
         ## Normalize image
-        image = (image - np.min(image)) / (np.max(image) - np.min(image) + 1e-10)
+        # image = (image - np.min(image)) / (np.max(image) - np.min(image) + 1e-10)           ## [0, 1]
+        image = 2 * ((image - np.min(image)) / (np.max(image) - np.min(image) + 1e-10)) - 1   ## [-1, 1]
 
         ## Arrange
         images = image[:, :, ::-1].astype(np.float32)  # bgr -> rgb
@@ -150,10 +163,66 @@ class PriceTagDataset(Dataset):
         image = Image.open(image_file)
         image_draw = ImageDraw.Draw(image)
         font = ImageFont.truetype('FreeMono.ttf', 20)
-        x1, y1 = bbox[:2]
-        x2, y2 = bbox[2:4]
-        x3, y3 = bbox[4:6]
-        x4, y4 = bbox[6:]
-        image_draw.polygon([(x1, y1), (x2, y2), (x3, y3), (x4, y4)], outline='red')
-        image_draw.text((x1, y1), transcription, fill='red', font=font)
+        if len(bbox) == 4:
+            x1, y1, x2, y2 = bbox
+            image_draw.polygon([(x1, y1), (x2, y1), (x2, y2), (x1, y2)], outline='red')
+            image_draw.text((x1, y1), transcription, fill='red', font=font)
+        elif len(bbox) == 8:
+            x1, y1 = bbox[:2]
+            x2, y2 = bbox[2:4]
+            x3, y3 = bbox[4:6]
+            x4, y4 = bbox[6:]
+            image_draw.polygon([(x1, y1), (x2, y2), (x3, y3), (x4, y4)], outline='red')
+            image_draw.text((x1, y1), transcription, fill='red', font=font)
         image.save(os.path.join(self.visualization_dir, os.path.basename(image_file)))
+
+class RandomRotate(object):
+    """Randomly rotates an image    
+    Bounding boxes which have an area of less than 25% in the remaining in the 
+    transformed image is dropped. The resolution is maintained, and the remaining
+    area if any is filled by black color.
+    Parameters
+    ----------
+    angle: float or tuple(float)
+        if **float**, the image is rotated by a factor drawn 
+        randomly from a range (-`angle`, `angle`). If **tuple**,
+        the `angle` is drawn randomly from values specified by the 
+        tuple
+    Returns
+    -------
+    numpy.ndarray
+        Rotated image in the numpy format of shape `HxWxC`
+    numpy.ndarray
+        Tranformed bounding box co-ordinates of the format `n x 4` where n is 
+        number of bounding boxes and 4 represents `x1,y1,x2,y2` of the box
+    """
+
+    def __init__(self, angle=10):
+        self.angle = angle
+        if type(self.angle) == tuple:
+            assert len(self.angle) == 2, "Invalid range"
+        else:
+            self.angle = (-self.angle, self.angle)
+
+    def __call__(self, img, bboxes):
+        angle = random.uniform(*self.angle)
+
+        w, h = img.shape[1], img.shape[0]
+        cx, cy = w // 2, h // 2
+
+        img = rotate_image(img, angle)
+        corners = get_corners(bboxes)
+    
+        corners = np.hstack((corners, bboxes[:, 4:]))
+        corners[:, :8] = rotate_box(corners[:, :8], angle, cx, cy, h, w)
+        new_bbox = get_enclosing_box(corners)
+
+        scale_factor_x = img.shape[1] / w
+        scale_factor_y = img.shape[0] / h
+        img = cv2.resize(img, (w, h))
+
+        new_bbox[:,:4] /= [scale_factor_x, scale_factor_y, scale_factor_x, scale_factor_y] 
+        bboxes  = new_bbox
+        bboxes = clip_box(bboxes, [0, 0, w, h], 0.25)
+
+        return img, bboxes